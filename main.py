ceb1961ee13cc7c011f6929d2197a04d0adcc046
filintodelgado <filintodelgado@gmail.com>
2022-12-21 04:23:41 +0000

    tamanho adcionado ao window

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 import customtkinter
 
 customtkinter.set_appearance_mode("system") # segue o tema do sistema
 
 # Creates the main window for customtkinter
 root = customtkinter.CTk()
+root.geometry("1280x720") # atribui um tamanho para quando não estiver maximizado
 root.attributes("-zoomed", True) # maximized
 
 def buttonClick():
