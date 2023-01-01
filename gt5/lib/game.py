04c6f1224095475ffdb9b299431435ff76c758e4
filintodelgado <filintodelgado@gmail.com>
2023-01-01 08:52:25 +0000

    routinas de favoritos e categorias adcionados e pequenos aranjos

--- gt5/lib/game.py
+++ gt5/lib/game.py
@@ -58,20 +58,10 @@ def get_id() -> int:
 id = get_id()
 
 
-def add_to_collection(__data:str, id):
-    """
-    Adciona a coleção do usuario
-    """
-    USERPATH = user.PASTA+"/"+__data["username"]
+def make_path(id):
+    global PASTA
 
-    userdata = utils.parser(USERPATH)
-
-    try:
-        userdata["games"].append(id)
-    except KeyError:
-        userdata["games"] = [id]
-    
-    utils.write(USERPATH, userdata)
+    return PASTA+"/"+id
 
 
 def criar(__data:dict) -> bool:
@@ -91,10 +81,17 @@ def criar(__data:dict) -> bool:
     """
     global id, PASTA
 
-    utils.write(f'{PASTA}/{id}', __data)
+    # cria a pasta de jogos caso ainda não exista
+    if PASTA not in os.listdir():
+        os.mkdir(PASTA)
 
-    add_to_collection(__data, id)
+    utils.write(make_path(id), __data)
 
+    # adciona a coleção do usuario
+    #   usa o username em __data
+    user.add_to_collection(__data["username"], id)
+
+    # incrementa o id
     increment_id()
 
 
@@ -126,37 +123,172 @@ def validar(id) -> int:
     return 0
 
 
-def favoritar(id, username):
+def get(id):
     """
-    Adciona o jogo a lista de favorito do usuario
+    Retorna os dados do jogo num dicionario
+    """
 
-    id: o id unico do jogo
-    username: o nome unico do usuario
+    PATH = make_path(id)
 
-    return 1: caso o id do jogo seja invalido
-    return 2: caso o username seja invalido
+    return utils.parser(PATH)
 
-    return 0: em sucesso
+
+def list_games() -> list:
+    """
+    Lista todos os jogos registrados
+    
+    :return: o id dos jogos numa lista
     """
     global PASTA
 
-    USERPATH = user.PASTA+"/"+username
+    return os.listdir(PASTA)
 
-    userdata = utils.parser(USERPATH)
 
-    # jogo não encontrado
-    if validar() != 0:
-        return 1
+def get_all_games(username=None) -> list:
+    """
+    Retorna todo os dados dos jogos registrados
+    """
+    games_data = list()
+
+    for id in list_games():
+        jogo = get(id)
+
+        # caso seja o dono do jogo
+        if jogo["username"] == username:
+            jogo["owner"] = True
+        else:
+            jogo["owner"] = False
+
+        # caso o jogo esteja nos favoritos
+        if username and id in user.get_favoritos(username):
+            jogo["favorito"] = True
+        else:
+            jogo["favorito"] = False
+
+        games_data.append(jogo)
+
+    return games_data
     
-    # caso o usuario não seja valido
-    if user.validar() != 0:
-        return 2
 
-    try:
-        userdata["favoritos"].append(id)
-    except KeyError:
-        userdata["favoritos"] = [id]
+
+def get_accessible_game(username:str=None) -> dict:
+    """
+    Retorna os jogos acessiveis ao usuario
+    """
+
+    games = list()
+
+    # para cada jogo
+    for jogo in get_all_games(username):
+
+        # caso pertença ao usuario ou seja publico
+        if jogo["username"] == username or not jogo["private"]:
+            games.append(jogo)
+
+    return games
+
+
+def remove(id):
+    # remove o jogo da coleção do usuario
+    user.remove_collection(get(id)["username"], id)
+
+    # deleta o arquivo
+    os.remove(make_path(id))
+
+
+def pesquisar(key=None, value=None):
+    """
+    Pesquisa todas os ocurencias correspondente aos argumentos.
+
+    :param key: pesquisa pelo atributo do jogo como nome por exemplo
+    :param value: pesquisa pelo valor do atributo
+
+    Caso o key seja uma lista o value tambem deverá ser uma lista com o
+    mesmo tamanho.
+    Quando a pesquisa é feita usando lista ela é uma pesquisa multipesquisa.
+    e o jogo precisa satisfaser todos os requisitos para ser uma pesquisa
+    valida.
+
+    ex: nome="Jogo teste 1"
+        -key-|----value----
+    """
+
+    # pesquisa todos os jogos
+    todos_os_jogos = get_all_games()
+
+    resultado = list()
+
+    # faz o error checking
+    if type(key) == str and type(value) == str:
+        for jogo in todos_os_jogos:
+            if jogo[key] == value:
+                resultado.append(jogo)
+    elif len(key) == len(value):
+        keys = key
+        values = value
+        for jogo in todos_os_jogos:
+            # o jogo precisar passar todos os testes 
+            teste = True
+            for key, value in keys, values:
+                if jogo[key] != value:
+                    teste = False
+            if teste:
+                resultado.append(jogo)
     
-    utils.write(USERPATH, userdata)
+    return resultado
 
 
+def delete_by_key(key:str, value) -> None:
+    """
+    Faz uma pesquisa e deleta os arquivos que a pesquisa retorna
+    """
+    resultado = pesquisar(key, value)
+
+    for jogo in resultado:
+        remove(jogo["id"])
+
+
+def get_categoria(username:str=None) -> dict:
+    jogos = get_accessible_game(username)
+
+    resultado = dict()
+
+    for jogo in jogos:
+        if jogo["categoria"] not in resultado:
+            resultado[jogo["categoria"]] = [jogo]
+        else:
+            resultado[jogo["categoria"]].append(jogo)
+
+
+def get_my_games(username:str) -> dict:
+    """
+    Retorna os dados dos jogos que o usuario criou
+    """
+    resultado = list()
+
+    for jogo in get_all_games(username):
+        if jogo["username"] == username:
+            resultado.append(jogo)
+    
+    return resultado
+
+
+def get_favoritos(username:str) -> list:
+    """
+    Retorna os dados dos jogos favoritos do usuario
+    """
+    resultado = list()
+
+
+    for jogo in get_all_games(username):
+        if jogo["favorito"]:
+            resultado.append(jogo)
+    
+    return resultado
+
+
+def delete_by_user(username:str) -> None:
+    """
+    Deleta todos os jogos criado pelo usuario
+    """
+    delete_by_key("username", username)
