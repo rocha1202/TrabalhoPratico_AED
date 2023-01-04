fbd1e472d4efb31786828303ad8fb0d5fd24d72c
filintodelgado <filintodelgado@gmail.com>
2023-01-04 15:54:58 +0000

    dark tema por padrão

--- gt5/widgets/button.py
+++ gt5/widgets/button.py
@@ -9,6 +9,9 @@ class Button(customtkinter.CTkButton):
   def __init__(self, master, text, theme, command=None):
     super().__init__(master, text=text, fg_color=theme["PRINCIPAL"], hover_color=theme["SECUNDARIA"], command=command)
 
+  def change_image(self, new_image):
+    self.configure(image=new_image)
+
 
 class ImageButton(customtkinter.CTkButton):
   def __init__(self, master, fg_color, path, size=(10, 10), command=None):
