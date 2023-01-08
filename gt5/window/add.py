c31b72882b9cd1fd72b25cc25bfc06dbade6f492
Filinto Delgado <filintodelgado@gmail.com>
2023-01-08 21:54:11 +0000

    window adcionais

---
+++ gt5/window/add.py
@@ -0,0 +1,10 @@
+import customtkinter
+
+def AddGame(master:customtkinter.CTk) -> customtkinter.CTkToplevel:
+  """
+  Cria uma window para adcionar um novo jogo
+  """
+  window = customtkinter.CTkToplevel(master)
+  window.focus_force()
+  window.title("Adcionar jogo")
+  window.geometry(f'500x300+{master.winfo_x()}+{master.winfo_y()}')
